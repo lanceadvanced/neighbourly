4dc78310e2902f3f1f34a00778e64977a888b223
lakickristina <kristina.lakic@stud.hslu.ch>
2024-04-26 00:35:06 +0200

    Adding model initializers

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,35 @@ Route::post('send-test-request', [TestClientController::class, 'sendTestRequest'
 Route::get('delete-sample-offer/{offerID}', [OfferController::class, 'deleteSampleOffer'])->name('delete-sample-offer');
 Route::post('create-sample-offer', [OfferController::class, 'createSampleOffer'])->name('create-sample-offer');
 
+Route::get('initialize', function () {
+    $city = new City();
+    $city->name = 'Bern';
+    $city->zip = '3014';
+    $city->save();
+
+    $address = new Address();
+    $address->street = 'Hilfikerstrasse';
+    $address->number = '4';
+    $address->fk_cityID = $city->getKey();
+    $address->save();
+
+    $community = new Community();
+    $community->fk_addressID = $address->getKey();
+    $community->managed = 0;
+    $community->save();
+
+    $user = new User();
+    $user->name = 'Vater';
+    $user->email = 'vater@muetter.cz';
+    $user->password = 'dinimuetter';
+    $user->save();
+
+    $account = new Account();
+    $account->fk_communityID = $community->getKey();
+    $account->fk_userID = $user->getKey();
+    $account->firstname = 'Vater';
+    $account->lastname = 'God';
+    $account->save();
+});
+
 require __DIR__ . '/auth.php';
